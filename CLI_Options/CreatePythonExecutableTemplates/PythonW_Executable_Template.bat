1742f9d788bbcffc6bbba96c476eb029a49c2230
DavyJones324 <alex.summers1@hotmail.com>
2025-11-21 09:58:49 -0600

    Add files via upload

---
+++ CLI_Options/CreatePythonExecutableTemplates/PythonW_Executable_Template.bat
@@ -0,0 +1,3 @@
+@echo off
+set CURRDIR=%~dp0
+start "FILENAME" "%CURRDIR%..\..\pythonw.exe" "%CURRDIR%FILENAME.pyw" %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ CLI_Options/CreatePythonExecutableTemplates/Python_Executable_Template.bat
@@ -0,0 +1,5 @@
+@echo off
+set PY_EXE="C:\CLI_Tools\Python\PythonXY\python.exe"
+set PY_SCRIPT_PATH="C:\CLI_Tools\Python\Python_Programs\%PROGRAM_NAME%\%SCRIPT_NAME%.py"
+%PY_EXE% %PY_SCRIPT_PATH%
+pause
